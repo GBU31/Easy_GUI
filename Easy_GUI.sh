2043dbc8299ae59c2c9f58bfd7f715504c3af9d7
brookehorizon <danieltashman17@gmail.com>
2022-03-21 11:12:04 -0600

    first commit

---
+++ Easy_GUI.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+
+git clone https://github.com/brookehorizon/EasyGUI && mv EasyGUI $1
---
+++ __main__.py
@@ -0,0 +1,6 @@
+from os import system
+
+
+if __name__ == '__main__':
+    project_name = input('project name:')
+    system(f'bash Easy_GUI.sh {project_name}')
