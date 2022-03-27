2190014281069ed0f2e06501ac20f5668a5f1f3d
brookehorizon <86805843+brookehorizon@users.noreply.github.com>
2022-03-27 01:54:02 -0600

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -3,4 +3,4 @@ from os import system
 
 if __name__ == '__main__':
     project_name = input('project name:')
-    system(f'bash Easy_GUI.sh {project_name}')
+    system(f'git clone https://github.com/brookehorizon/EasyGUI && mv EasyGUI {project_name}')
